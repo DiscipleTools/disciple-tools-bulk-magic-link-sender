8ec1f0377030d44592f4281e3cd3ca6cbb415f5f
kodinkat <me@karlkatewu.com>
2021-11-09 11:10:24 +0000

    Updated cron run interval time + Enabled update checker

--- disciple-tools-magic-links.php
+++ disciple-tools-magic-links.php
@@ -299,21 +299,21 @@ if ( ! function_exists( "dt_hook_ajax_notice_handler" ) ) {
  * Also, see the instructions for version updating to understand the steps involved.
  * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
  */
-//add_action( 'plugins_loaded', function (){
-//    if ( is_admin() ){
-//        // Check for plugin updates
-//        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-//            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
-//                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-//            }
-//        }
-//        if ( class_exists( 'Puc_v4_Factory' ) ){
-//            Puc_v4_Factory::buildUpdateChecker(
-//                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-magic-links/master/version-control.json',
-//                __FILE__,
-//                'disciple-tools-magic-links'
-//            );
-//
-//        }
-//    }
-//} );
+add_action( 'plugins_loaded', function () {
+    if ( is_admin() && ! ( is_multisite() && class_exists( "DT_Multisite" ) ) || wp_doing_cron() ) {
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' ) ) {
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ) {
+            Puc_v4_Factory::buildUpdateChecker(
+                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-magic-links/master/version-control.json',
+                __FILE__,
+                'disciple-tools-magic-links'
+            );
+
+        }
+    }
+} );
