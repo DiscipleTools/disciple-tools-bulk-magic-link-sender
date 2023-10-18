7ca8570dedac1d730e7466becd3fc80372ab72c7
kodinkat <me@karlkatewu.com>
2023-10-18 17:38:08 +0100

    Added user login app magic link type

---
+++ magic-link/magic-link-user-login-app.php
@@ -0,0 +1,286 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
+
+
+/**
+ * Class Disciple_Tools_Magic_Links_Magic_User_Login_App
+ */
+class Disciple_Tools_Magic_Links_Magic_User_Login_App extends DT_Magic_Url_Base {
+
+    public $page_title = 'User Login App';
+    public $page_description = 'Login User App - Magic Links.';
+    public $root = 'my'; // @todo define the root of the url {yoursite}/root/type/key/action
+    public $type = 'apps'; // @todo define the type
+    public $post_type = 'user';
+    private $meta_key = '';
+    public $show_bulk_send = false;
+    public $show_app_tile = false;
+
+    private static $_instance = null;
+    public $meta = []; // Allows for instance specific data.
+
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    } // End instance()
+
+    public function __construct() {
+
+        /**
+         * Specify metadata structure, specific to the processing of current
+         * magic link type.
+         *
+         * - meta:              Magic link plugin related data.
+         *      - app_type:     Flag indicating type to be processed by magic link plugin.
+         *      - post_type     Magic link type post type.
+         *      - contacts_only:    Boolean flag indicating how magic link type user assignments are to be handled within magic link plugin.
+         *                          If True, lookup field to be provided within plugin for contacts only searching.
+         *                          If false, Dropdown option to be provided for user, team or group selection.
+         *      - fields:       List of fields to be displayed within magic link frontend form.
+         */
+        $this->meta = [
+            'app_type'      => 'magic_link',
+            'post_type'     => $this->post_type,
+            'contacts_only' => false,
+            'fields'        => [
+                [
+                    'id'    => 'name',
+                    'label' => 'Name'
+                ]
+            ]
+        ];
+
+        $this->meta_key = $this->root . '_' . $this->type . '_magic_key';
+        parent::__construct();
+
+        /**
+         * user_app and module section
+         */
+        add_filter( 'dt_settings_apps_list', [ $this, 'dt_settings_apps_list' ], 10, 1 );
+        add_action( 'rest_api_init', [ $this, 'add_endpoints' ] );
+
+        /**
+         * tests if other URL
+         */
+        $url = dt_get_url_path();
+        if ( strpos( $url, $this->root . '/' . $this->type ) === false ) {
+            return;
+        }
+
+        /**
+         * tests magic link parts are registered and have valid elements
+         */
+        if ( !$this->check_parts_match( false ) ){
+            return;
+        }
+
+        if ( !is_user_logged_in() ) {
+            /* redirect user to login page with a redirect_to back to here */
+            wp_redirect( dt_login_url( 'login', '?redirect_to=' . rawurlencode( site_url( dt_get_url_path() ) ) ) );
+            exit;
+        }
+
+        // load if valid url
+        add_action( 'dt_blank_body', [ $this, 'body' ] );
+        add_filter( 'dt_magic_url_base_allowed_css', [ $this, 'dt_magic_url_base_allowed_css' ], 10, 1 );
+        add_filter( 'dt_magic_url_base_allowed_js', [ $this, 'dt_magic_url_base_allowed_js' ], 10, 1 );
+
+    }
+
+    public function dt_magic_url_base_allowed_js( $allowed_js ) {
+        // @todo add or remove js files with this filter
+        return $allowed_js;
+    }
+
+    public function dt_magic_url_base_allowed_css( $allowed_css ) {
+        // @todo add or remove js files with this filter
+        return $allowed_css;
+    }
+
+    /**
+     * Builds magic link type settings payload:
+     * - key:               Unique magic link type key; which is usually composed of root, type and _magic_key suffix.
+     * - url_base:          URL path information to map with parent magic link type.
+     * - label:             Magic link type name.
+     * - description:       Magic link type description.
+     * - settings_display:  Boolean flag which determines if magic link type is to be listed within frontend user profile settings.
+     *
+     * @param $apps_list
+     *
+     * @return mixed
+     */
+    public function dt_settings_apps_list( $apps_list ) {
+        $apps_list[ $this->meta_key ] = [
+            'key'              => $this->meta_key,
+            'url_base'         => $this->root . '/' . $this->type,
+            'label'            => $this->page_title,
+            'description'      => $this->page_description,
+            'settings_display' => true
+        ];
+
+        return $apps_list;
+    }
+
+    /**
+     * Writes custom styles to header
+     *
+     * @see DT_Magic_Url_Base()->header_style() for default state
+     * @todo remove if not needed
+     */
+    public function header_style(){
+        ?>
+        <style>
+            body {
+                background-color: white;
+                padding: 1em;
+            }
+
+            .app-container {
+                display: flex;
+                flex-wrap: wrap;
+            }
+            .app {
+                padding: 1em;
+                margin: 1em;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+            }
+        </style>
+        <?php
+    }
+
+    /**
+     * Writes javascript to the header
+     *
+     * @see DT_Magic_Url_Base()->header_javascript() for default state
+     * @todo remove if not needed
+     */
+    public function header_javascript(){
+        ?>
+        <script>
+            console.log('insert header_javascript')
+        </script>
+        <?php
+    }
+
+    /**
+     * Writes javascript to the footer
+     *
+     * @see DT_Magic_Url_Base()->footer_javascript() for default state
+     * @todo remove if not needed
+     */
+    public function footer_javascript(){
+        ?>
+        <script>
+            console.log('insert footer_javascript')
+            let jsObject = [<?php echo json_encode([
+                'map_key' => DT_Mapbox_API::get_key(),
+                'root' => esc_url_raw( rest_url() ),
+                'nonce' => wp_create_nonce( 'wp_rest' ),
+                'parts' => $this->parts,
+                'translations' => [
+                    'add' => __( 'Add Magic', 'disciple-tools-plugin-starter-template' ),
+                ],
+            ]) ?>][0];
+
+            console.log(jsObject);
+        </script>
+        <?php
+        return true;
+    }
+
+    public function body(){
+
+        // As we required the user to login before they could access this app,
+        // we now have access to the logged in user record
+
+        $user_id = get_current_user_id();
+        $display_name = dt_get_user_display_name( $user_id );
+        $user_contact = Disciple_Tools_Users::get_contact_for_user( $user_id );
+
+        // We also know who owns this user app
+        $app_owner_id = $this->parts['post_id'];
+        $app_owner = get_user_by( 'ID', $app_owner_id );
+        $app_owner_display_name = dt_get_user_display_name( $app_owner_id );
+
+        // @todo Create an app here that interacts with both the logged in user and the user who owns the app
+        $apps_list = apply_filters( 'dt_settings_apps_list', $apps_list = [] );
+
+        ?>
+        <div id="custom-style"></div>
+        <div id="wrapper">
+            <div class="grid-x">
+                <div class="cell center">
+                    <h2 id="title">Hello there <?php echo esc_html( $display_name ) ?></h2>
+                </div>
+            </div>
+            <hr>
+            <div id="content">
+                <h3>Your Apps</h3>
+                <div class="app-container">
+                    <?php
+                    foreach ( $apps_list as $app ) {
+                        if ( $app['settings_display'] ){
+                            $app_user_key = get_user_option( $app['key'] );
+                            $app_url_base = trailingslashit( trailingslashit( site_url() ) . $app['url_base'] );
+                            if ( !$app_user_key ){
+                                continue;
+                            }
+                            $app_link = $app_url_base . $app_user_key;
+                            ?>
+                            <a class="app" target="_blank" href="<?php echo esc_url( $app_link ) ?>"><?php echo esc_html( $app['label'] ) ?></a>
+                            <?php
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register REST Endpoints
+     * @link https://github.com/DiscipleTools/disciple-tools-theme/wiki/Site-to-Site-Link for outside of wordpress authentication
+     */
+    public function add_endpoints() {
+        $namespace = $this->root . '/v1';
+        register_rest_route(
+            $namespace, '/'.$this->type, [
+                [
+                    'methods'  => 'GET',
+                    'callback' => [ $this, 'endpoint_get' ],
+                    'permission_callback' => function( WP_REST_Request $request ){
+                        $magic = new DT_Magic_URL( $this->root );
+
+                        return $magic->verify_rest_endpoint_permissions_on_post( $request );
+                    },
+                ],
+            ]
+        );
+        register_rest_route(
+            $namespace, '/'.$this->type, [
+                [
+                    'methods'  => 'POST',
+                    'callback' => [ $this, 'update_record' ],
+                    'permission_callback' => function( WP_REST_Request $request ){
+                        $magic = new DT_Magic_URL( $this->root );
+
+                        return $magic->verify_rest_endpoint_permissions_on_post( $request );
+                    },
+                ],
+            ]
+        );
+    }
+
+    public function update_record( WP_REST_Request $request ) {
+        return true;
+    }
+
+    public function endpoint_get( WP_REST_Request $request ) {
+        return [];
+    }
+}
+Disciple_Tools_Magic_Links_Magic_User_Login_App::instance();
