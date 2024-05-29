87ee09758a2c7263c10ec6d51847a1fba365d014
corsacca <corsacca@gmail.com>
2024-05-29 09:48:21 -0500

    v1.22.1

--- disciple-tools-bulk-magic-link-sender.php
+++ disciple-tools-bulk-magic-link-sender.php
@@ -5,7 +5,7 @@
  * Description: Disciple Tools - Magic Links for users, contacts, groups and teams assignment + schedule management for magic links dispatching over configured sending channels.
  * Text Domain: disciple-tools-bulk-magic-link-sender
  * Domain Path: /languages
- * Version:  1.22.0
+ * Version:  1.22.1
  * Author URI: https://github.com/DiscipleTools
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-bulk-magic-link-sender
  * Requires at least: 4.7.0
