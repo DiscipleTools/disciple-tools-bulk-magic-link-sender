64420f44b1db8bac6477d2f299daa74cc5a755db
bert958 <97952090+bert958@users.noreply.github.com>
2024-09-12 15:57:29 +0200

    Update magic-link-user-app.php
    A filter would for example allow to sort the assigned posts on name

--- magic-link/magic-link-user-app.php
+++ magic-link/magic-link-user-app.php
@@ -867,6 +867,8 @@ class Disciple_Tools_Magic_Links_Magic_User_App extends DT_Magic_Url_Base {
             }
         }
 
+        $data = apply_filters( 'dt_smart_links_filter_assigned_posts_in_contact_updates', $data ); // e.g. to sort alphabetically on name
+
         return $data;
     }
 
