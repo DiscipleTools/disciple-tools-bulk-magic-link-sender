6b3756d93950f29f0877aba9cb4cde60247fe656
adventureit <adventure4it@gmail.com>
2025-03-27 20:37:02 +0200

    DT - Magic Link - list-sub-assigned-records - language modal
    Fixing issues on css, js, button's name and alignment and renaming files.

--- magic-link/templates/single-record.php
+++ magic-link/templates/single-record.php
@@ -131,22 +131,27 @@ class Disciple_Tools_Magic_Links_Template_Single_Record extends DT_Magic_Url_Bas
         }
 
         // Support Typeahead APIs
-        $path     = '../../assets/';
-        $path_js  = $path . 'single-connections.js';
-        $path_css = $path . 'single-connections.css';
+        $path     = '/dt-core/dependencies/typeahead/dist/';
+        $path_js  = $path . 'jquery.typeahead.min.js';
+        $path_css = $path . 'jquery.typeahead.min.css';
 
-        wp_enqueue_script( 'single-connections', plugin_dir_url( __FILE__ ) . $path_js, [ 'jquery' ], filemtime(  __FILE__ ) . $path_js );
+        $path_sr     = '../../assets/';
+        $path_sr_js  = $path_sr . 'single-record.js';
+        $path_sr_css = $path_sr . 'single-record.css';
 
-        wp_enqueue_style( 'single-connections-css', plugin_dir_url( __FILE__ ) . $path_css, null, filemtime(  __FILE__ ) . $path_css ) ;
+        $dtwc_version = '0.7.4';        
 
-        $dtwc_version = '0.7.4';
+        wp_enqueue_script( 'jquery-typeahead', get_template_directory_uri() . $path_js, [ 'jquery' ], filemtime( get_template_directory() . $path_js ) );
+        wp_enqueue_style( 'jquery-typeahead-css', get_template_directory_uri() . $path_css, [], filemtime( get_template_directory() . $path_css ) );
+        wp_enqueue_style( 'material-font-icons-css', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.6.96/css/materialdesignicons.min.css', [], '6.6.96' );
+
+        wp_enqueue_script( 'single-record', plugin_dir_url( __FILE__ ) . $path_sr_js, [ 'jquery' ], filemtime(  __FILE__ ) . $path_sr_js );
+
+        wp_enqueue_style( 'single-record-css', plugin_dir_url( __FILE__ ) . $path_sr_css, null, filemtime(  __FILE__ ) . $path_sr_css ) ;
 
         wp_enqueue_style( 'dt-web-components-css', "https://cdn.jsdelivr.net/npm/@disciple.tools/web-components@$dtwc_version/styles/light.css", [], $dtwc_version );
         
-        wp_enqueue_script( 'dt-web-components-js', "https://cdn.jsdelivr.net/npm/@disciple.tools/web-components@$dtwc_version/dist/index.js", $dtwc_version );
-
-        $mdi_version = '6.6.96';
-        wp_enqueue_style( 'material-font-icons-css', "https://cdn.jsdelivr.net/npm/@mdi/font@$mdi_version/css/materialdesignicons.min.css", [], $mdi_version );        
+        wp_enqueue_script( 'dt-web-components-js', "https://cdn.jsdelivr.net/npm/@disciple.tools/web-components@$dtwc_version/dist/index.js", $dtwc_version );      
 
         Disciple_Tools_Bulk_Magic_Link_Sender_API::enqueue_magic_link_utilities_script();
     }
@@ -158,7 +163,8 @@ class Disciple_Tools_Magic_Links_Template_Single_Record extends DT_Magic_Url_Bas
         $allowed_js[] = 'mapbox-cookie';
         $allowed_js[] = 'mapbox-search-widget';
         $allowed_js[] = 'google-search-widget';
-        $allowed_js[] = 'single-connections';           
+        $allowed_js[] = 'jquery-typeahead';        
+        $allowed_js[] = 'single-record';           
         $allowed_js[] = 'dt-web-components-js';               
 
         $allowed_js[] = Disciple_Tools_Bulk_Magic_Link_Sender_API::get_magic_link_utilities_script_handle();
@@ -170,8 +176,9 @@ class Disciple_Tools_Magic_Links_Template_Single_Record extends DT_Magic_Url_Bas
         // @todo add or remove js files with this filter
         // example: $allowed_css[] = 'your-enqueue-handle';
         $allowed_css[] = 'mapbox-gl-css';
-        $allowed_css[] = 'material-font-icons-css';
-        $allowed_css[] = 'single-connections-css';   
+        $allowed_css[] = 'jquery-typeahead-css';
+        $allowed_css[] = 'material-font-icons-css';        
+        $allowed_css[] = 'single-record-css';   
         $allowed_css[] = 'dt-web-components-css';
 
         return $allowed_css;
@@ -1288,7 +1295,7 @@ class Disciple_Tools_Magic_Links_Template_Single_Record extends DT_Magic_Url_Bas
                         <b>
                             <?php echo esc_html( $has_title ? $this->adjust_template_title_translation( $this->template['title'], $this->template['title_translations'] ) : '' ); ?>
 
-                            <button class="mdi mdi mdi-web" onclick="document.getElementById('single-record-locale-modal')._openModal()"></button> 
+                            <button class="locale-button mdi mdi-web" onclick="document.getElementById('single-record-locale-modal')._openModal()"></button> 
                         </b>
                     </h2>
                 </div>
@@ -1301,7 +1308,8 @@ class Disciple_Tools_Magic_Links_Template_Single_Record extends DT_Magic_Url_Bas
                 
             }
                 $lang = dt_get_available_languages();
-                $current_lang = trim( wp_get_current_user()->locale );               
+                $current_lang = trim( wp_get_current_user()->locale ); 
+
             ?>
             <div id="content">
                 <div id="alert_notice" style="display: none; border-style: solid; border-width: 2px; border-color: #4caf50; background-color: rgba(142,195,81,0.2); border-radius: 5px; padding: 2em; margin: 1em 0">
