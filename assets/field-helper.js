d5219760632de96aac89d5a0aa3cc23c6567653f
corsacca <corsacca@gmail.com>
2025-08-11 13:07:21 +0100

    Shared function for setting form data.

--- assets/field-helper.js
+++ assets/field-helper.js
@@ -70,6 +70,24 @@ jQuery(document).on('click', 'button.add-button', function(e) {
     }
 });
 
+// Clear dt-date component similar to create-record (updateTimestamp(''))
+jQuery(document).on('click', '.clear-date-button', function (evt) {
+    const tr = jQuery(this).closest('tr');
+    const inputId = jQuery(this).data('inputid');
+    if (!inputId) return;
+    const comp = tr.find('#' + inputId);
+    if (comp.length) {
+        const el = comp.get(0);
+        if (el && typeof el.updateTimestamp === 'function') {
+            try { el.updateTimestamp(''); } catch (e) {}
+        }
+        comp.attr('value', '');
+    } else {
+        tr.find('#' + inputId).val('');
+    }
+    tr.find('#form_content_table_field_meta').val('');
+});
+
 // Collect DT and custom fields from the Single Record UI
 if (!window.SHAREDFUNCTIONS.collectFields) {
     window.SHAREDFUNCTIONS.collectFields = function (options) {
@@ -354,5 +372,224 @@ if (!window.SHAREDFUNCTIONS.collectFields) {
         });
 
         return payloadFields;
+    };
+}
+
+// Set DT and custom field values in the Single Record UI from a post object
+if (!window.SHAREDFUNCTIONS.setFieldsFromPost) {
+    window.SHAREDFUNCTIONS.setFieldsFromPost = function (post) {
+        if (!post) return;
+
+        jQuery('.form-content-table > tbody > tr').each(function (idx, tr) {
+            const field_id = jQuery(tr).find('#form_content_table_field_id').val();
+            const field_type = jQuery(tr).find('#form_content_table_field_type').val();
+            const field_template_type = jQuery(tr).find('#form_content_table_field_template_type').val();
+            const field_meta = jQuery(tr).find('#form_content_table_field_meta');
+            const selector = '#' + field_id;
+
+            if (field_template_type !== 'dt') {
+                jQuery(tr).find(selector).val('');
+                return;
+            }
+
+            switch (field_type) {
+                case 'number':
+                case 'textarea':
+                case 'text': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    if (dtComponent.length) {
+                        dtComponent.attr('value', post[field_id] ? post[field_id] : '');
+                    } else {
+                        jQuery(tr).find(selector).val(post[field_id] ? post[field_id] : '');
+                    }
+                    break;
+                }
+                case 'link': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    let valueToSet = '';
+                    if (post[field_id] !== undefined && post[field_id] !== null) {
+                        const v = post[field_id];
+                        if (typeof v === 'string') {
+                            valueToSet = v;
+                        } else if (Array.isArray(v)) {
+                            // expect array of { value, type? }
+                            valueToSet = JSON.stringify({ values: v });
+                        } else if (typeof v === 'object') {
+                            // object shape, use as-is
+                            try { valueToSet = JSON.stringify(v); } catch (e) { valueToSet = ''; }
+                        }
+                    }
+                    if (dtComponent.length) {
+                        dtComponent.attr('value', valueToSet);
+                    } else {
+                        jQuery(tr).find(selector).val(typeof valueToSet === 'string' ? valueToSet : '');
+                    }
+                    // Clear any dynamic link inputs/sections for a clean slate
+                    jQuery(tr).find('.link-input').val('');
+                    jQuery(tr).find('.link-section').remove();
+                    break;
+                }
+                case 'key_select': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    const keyValue = (post[field_id] && post[field_id]['key']) ? post[field_id]['key'] : '';
+                    if (dtComponent.length) {
+                        dtComponent.attr('value', keyValue);
+                    } else {
+                        jQuery(tr).find(selector).val(keyValue);
+                    }
+                    break;
+                }
+                case 'communication_channel': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    if (dtComponent.length) {
+                        const values = Array.isArray(post[field_id]) ? post[field_id].map(function (v) { return { value: v.value || '' }; }) : [];
+                        dtComponent.attr('value', JSON.stringify(values));
+                        field_meta.val('');
+                    } else {
+                        jQuery(tr).find('.channel-delete-button[data-field="' + field_id + '"]').each(function (idx, del_button) {
+                            jQuery(del_button).parent().parent().remove();
+                        });
+                        if (post[field_id]) {
+                            post[field_id].forEach(function (option) {
+                                jQuery(tr).find('button.add-button').trigger('click');
+                                jQuery(tr).find('.input-group').last().find('input').val(option['value']);
+                                if (option['key']) {
+                                    jQuery(tr).find('.input-group').last().find('button').attr('data-key', window.lodash.escape(option['key']));
+                                }
+                            });
+                        }
+                        field_meta.val('');
+                    }
+                    break;
+                }
+                case 'multi_select': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    if (dtComponent.length) {
+                        const selected = Array.isArray(post[field_id]) ? post[field_id] : [];
+                        dtComponent.attr('value', JSON.stringify(selected));
+                    } else {
+                        jQuery(tr).find('button').each(function () {
+                            jQuery(this).addClass('empty-select-button');
+                            jQuery(this).removeClass('selected-select-button');
+                            if (post[field_id] && (jQuery(this).data('field-key') === field_id) && post[field_id].includes(jQuery(this).attr('id'))) {
+                                jQuery(this).removeClass('empty-select-button');
+                                jQuery(this).addClass('selected-select-button');
+                            }
+                        });
+                    }
+                    break;
+                }
+                case 'boolean': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    if (dtComponent.length) {
+                        dtComponent.attr('value', !!post[field_id]);
+                    } else {
+                        jQuery(tr).find(selector).prop('checked', post[field_id]);
+                    }
+                    break;
+                }
+                case 'date': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    document.querySelector('dt-date').updateTimestamp('')
+                    if (dtComponent.length) {
+                        const formatted = (post[field_id] && post[field_id]['formatted']) ? post[field_id]['formatted'] : '';
+                        dtComponent.attr('value', formatted);
+                        field_meta.val(formatted);
+                    } else {
+                        if (post[field_id] && post[field_id]['timestamp']) {
+                            let timestamp = post[field_id]['timestamp'];
+                            jQuery(tr).find(selector).data('daterangepicker').setStartDate(moment.unix(timestamp));
+                            jQuery(tr).find(selector).val(moment.unix(timestamp).format('MMMM D, YYYY'));
+                            field_meta.val(moment.unix(timestamp).format('YYYY-MM-DD'));
+                        } else {
+                            jQuery(tr).find(selector).val('');
+                            field_meta.val('');
+                        }
+                    }
+                    break;
+                }
+                case 'tags': {
+                    const dtComponent = jQuery(tr).find('[id="' + field_id + '"]');
+                    if (dtComponent.length) {
+                        const tags = Array.isArray(post[field_id]) ? post[field_id] : [];
+                        dtComponent.attr('value', JSON.stringify(tags));
+                        field_meta.val('');
+                    } else {
+                        jQuery(tr).find('span.typeahead__cancel-button').trigger('click');
+                        let typeahead_tags_field_input = '.js-typeahead-' + field_id;
+                        let typeahead_tags = window.Typeahead[typeahead_tags_field_input];
+                        typeahead_tags.items = [];
+                        typeahead_tags.comparedItems = [];
+                        typeahead_tags.label.container.empty();
+                        if (post[field_id] && typeahead_tags) {
+                            post[field_id].forEach(function (tag) {
+                                typeahead_tags.addMultiselectItemLayout({ name: window.lodash.escape(tag) });
+                                typeahead_tags.adjustInputSize();
+                            });
+                        }
+                        field_meta.val('');
+                    }
+                    break;
+                }
+                case 'location': {
+                    let typeahead_location_field_input = '.js-typeahead-' + field_id;
+                    let typeahead_location = window.Typeahead[typeahead_location_field_input];
+                    if ( typeahead_location ) {
+                        typeahead_location.items = [];
+                        typeahead_location.comparedItems = [];
+                        typeahead_location.label.container.empty();
+                        if (post[field_id] ) {
+                            post[field_id].forEach(function (location) {
+                                typeahead_location.addMultiselectItemLayout({ ID: location['id'], name: window.lodash.escape(location['label']) });
+                            });
+                        }
+                        setTimeout(function () { typeahead_location.adjustInputSize(); }, 1);
+                    }
+                    field_meta.val('');
+                    break;
+                }
+                case 'location_meta': {
+                    jQuery(tr).find('#mapbox-search').val('');
+                    const deleteButtons = jQuery(tr).find('#location-grid-meta-results .mapbox-delete-button');
+                    if (deleteButtons.length) {
+                        deleteButtons.each(function(idx, button) {
+                            jQuery(button).parent().parent().remove();
+                        });
+                    }
+                    let lgm_results = jQuery(tr).find('#location-grid-meta-results');
+                    if (post[field_id] !== undefined && post[field_id].length !== 0) {
+                        jQuery.each(post[field_id], function (i, v) {
+                            if (v.grid_meta_id) {
+                                lgm_results.append('<div class="input-group">\
+                                    <input type="text" class="active-location input-group-field" id="location-' + window.lodash.escape(v.grid_meta_id) + '" dir="auto" value="' + window.lodash.escape(v.label) + '" readonly />\
+                                    <div class="input-group-button">\
+                                      <button type="button" class="button success delete-button-style open-mapping-grid-modal" title="' + window.lodash.escape(jsObject["mapbox"]["translations"]["open_modal"]) + '" data-id="' + window.lodash.escape(v.grid_meta_id) + '"><i class="fi-map"></i></button>\
+                                      <button type="button" class="button alert delete-button-style delete-button mapbox-delete-button" title="' + window.lodash.escape(jsObject["mapbox"]["translations"]["delete_location"]) + '" data-id="' + window.lodash.escape(v.grid_meta_id) + '">&times;</button>\
+                                    </div>\
+                                  </div>');
+                            } else {
+                                lgm_results.append('<div class="input-group">\
+                                    <input type="text" class="dt-communication-channel input-group-field" id="' + window.lodash.escape(v.key) + '" value="' + window.lodash.escape(v.label) + '" dir="auto" data-field="contact_address" />\
+                                    <div class="input-group-button">\
+                                      <button type="button" class="button success delete-button-style open-mapping-address-modal"\
+                                          title="' + window.lodash.escape(jsObject["mapbox"]["translations"]["open_modal"]) + '"\
+                                          data-id="' + window.lodash.escape(v.key) + '"\
+                                          data-field="contact_address"\
+                                          data-key="' + window.lodash.escape(v.key) + '">\
+                                          <i class="fi-pencil"></i>\
+                                      </button>\
+                                      <button type="button" class="button alert input-height delete-button-style channel-delete-button delete-button" title="' + window.lodash.escape(jsObject["mapbox"]["translations"]["delete_location"]) + '" data-id="' + window.lodash.escape(v.key) + '" data-field="contact_address" data-key="' + window.lodash.escape(v.key) + '">&times;</button>\
+                                    </div>\
+                                  </div>');
+                            }
+                        });
+                    }
+                    field_meta.val('');
+                    break;
+                }
+                default:
+                    break;
+            }
+        });
     };
 }
