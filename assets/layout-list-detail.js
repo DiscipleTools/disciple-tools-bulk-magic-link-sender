b532b8a187a366bf6e8e0d0185ef12baa30d5ec8
corsacca <corsacca@gmail.com>
2025-11-11 15:24:54 +0100

    Allow more than two comments to be displayed.

--- assets/layout-list-detail.js
+++ assets/layout-list-detail.js
@@ -403,7 +403,7 @@ function setComments(commentsTile, id) {
     sys_type: jsObject.sys_type,
     post_id: id,
     post_type: jsObject.template.record_type,
-    comment_count: 2,
+    comment_count: jsObject.template.show_recent_comments || 2,
   }
 
   const commentURL = window.apiRoot + '/post';
